33788ce120a70b3b3b49b423616773496071089e
surya <the.cuya@gmail.com>
2021-05-03 11:27:28 +0700

    initial state

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+// import {  } from "elements/Button";
+export default class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
